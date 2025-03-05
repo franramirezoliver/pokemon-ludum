35b5753aa5f383f786bfc278644529dd3f087bae
Francisco Ramírez <ramirezoliverf@gmail.com>
2025-03-05 18:06:09 +0100

    Pantalla del juego de pokemon

---
+++ src/modulorum/pokemon/components/PokemonOptiones.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li>Pokèmon 1</li>
+    <li>Pokèmon 2</li>
+    <li>Pokèmon 3</li>
+    <li>Pokèmon 4</li>
+  </ul>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+@import 'tailwindcss';
+
+li {
+  @apply bg-white shadow-md rounded-lg p-3 my-2 cursor-pointer w-40 text-center hover:bg-gray-100 transition-all;
+}
+</style>
