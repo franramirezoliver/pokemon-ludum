975a35ea5c74a5fa6da2f63e65a5911ccea72726
Francisco Ramírez <ramirezoliverf@gmail.com>
2025-03-12 18:21:22 +0100

    Opciones con botones y confeti

--- src/modulorum/pokemon/composables/usePokemonLudum.ts
+++ src/modulorum/pokemon/composables/usePokemonLudum.ts
@@ -1,6 +1,7 @@
 import { computed, onMounted, ref } from 'vue';
 import { LudumStatus, type Pokemon, type PokemonListaResponsio } from '../interfaces';
 import { pokemonApi } from '@/api/pokemonApi';
+import confetti from 'canvas-confetti';
 
 export const usePokemonLudum = () => {
   const ludumStatus = ref<LudumStatus>(LudumStatus.Ludit);
@@ -34,6 +35,21 @@ export const usePokemonLudum = () => {
     pokemons.value = pokemons.value.slice(quot);
   };
 
+  const examineResponsio = (id: number) => {
+    const vicit = temerePokemon.value.id === id;
+    if (vicit) {
+      ludumStatus.value = LudumStatus.Vicit;
+      confetti({
+        particleCount: 300,
+        spread: 150,
+        origin: { y: 0.6 },
+      });
+      return;
+    }
+
+    ludumStatus.value = LudumStatus.Perdidit;
+  };
+
   onMounted(async () => {
     await new Promise((r) => setTimeout(r, 500));
     pokemons.value = await obtinePokemons();
@@ -46,5 +62,6 @@ export const usePokemonLudum = () => {
     pokemonOptiones,
     sequentiOptiones,
     temerePokemon,
+    examineResponsio,
   };
 };
--- src/modulorum/pokemon/paginae/PokemonLudum.vue
+++ src/modulorum/pokemon/paginae/PokemonLudum.vue
@@ -4,17 +4,18 @@
     class="flex justify-center items-center flex-col w-screen h-screen"
   >
     <h1 class="text-3xl">Espere, por favor</h1>
-    <h3 class="animate-pulse">Cargando Pokèmons</h3>
+    <h3 class="animate-pulse">Cargando Pokémons</h3>
   </section>
 
   <section v-else class="flex justify-center items-center flex-col w-screen h-screen">
-    <h1 class="m-5">¿Quién es este Pokèmon?</h1>
-    <h3>{{ temerePokemon }}</h3>
+    <h1 class="m-5">¿Quién es este Pokémon?</h1>
+    <!-- <h3>{{ temerePokemon }}</h3> -->
+    <h3 class="capitalize my-3">{{ ludumStatus }}</h3>
     <PokemonImago
       :pokemon-id="temerePokemon.id"
       :revelare-pokemon="ludumStatus !== LudumStatus.Ludit"
     />
-    <PokemonOptiones />
+    <PokemonOptiones :optiones="optiones" @electus-optio="examineResponsio" />
   </section>
 </template>
 
@@ -24,7 +25,17 @@ import PokemonOptiones from '../components/PokemonOptiones.vue';
 import { usePokemonLudum } from '../composables/usePokemonLudum';
 import { LudumStatus } from '../interfaces';
 
-const { ludumStatus, estPortat, temerePokemon } = usePokemonLudum();
+const {
+  ludumStatus,
+  estPortat,
+  temerePokemon,
+  pokemonOptiones: optiones,
+  examineResponsio,
+} = usePokemonLudum();
+
+// const cumElectusOptio = (valorem: number) => {
+//   console.log(valorem);
+// };
 </script>
 
 <style scoped></style>
